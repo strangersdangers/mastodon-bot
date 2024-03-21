67e072c0b208cdb3c03be59eab5cd2bdf0a85855
strangersdangers <161281791+strangersdangers@users.noreply.github.com>
2024-03-21 02:33:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from thingtons import keep_alive
 
 mastodon = Mastodon(api_base_url = os.getenv('INSTURL'), access_token = os.getenv('TOKEN'))
 
-mastodon.toot('Working!!')
+#mastodon.toot('Working!!')
+mastodon.media_post("https://images.dog.ceo/breeds/cattledog-australian/IMG_5177.jpg", "image/jpeg")
 keep_alive()
