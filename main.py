2af76a7d75e4b54c4ddb002f1568ff3b9bb5f167
strangersdangers <161281791+strangersdangers@users.noreply.github.com>
2024-03-21 04:26:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 from mastodon import Mastodon
 import os
-from thingtons import keep_alive
+#from thingtons import keep_alive
+from flask import Flask
+import request
+import requests
 
+app = Flask(__name__)
 mastodon = Mastodon(api_base_url = os.getenv('INSTURL'), access_token = os.getenv('TOKEN'))
 
-#mastodon.toot('Working!!')
-mastodon.media_post("https://images.dog.ceo/breeds/cattledog-australian/IMG_5177.jpg", "image/jpeg")
-keep_alive()
+@app.route('/')
+def home():
+  return "<h1>" + "Website proudly hosted by cyclic!" + "</h1>"
+  #end
+@app.route('/post')
+def post():
+  type = request.args.get('type')
+  quote = []
+  data = []
+  randomquote = []
+  dogurl = []
+  imagetoget = []
+  data2 = []
+  if type == 'text':
+    randomquote = requests.get("https://api.quotable.io/quotes/random")
+    data = randomquote.json()
+    quote = 'woke up from my break just to post this. heres a quote for yall: ' + data['content'] + ' -' + data['author']
+    mastodon.toot(quote)
+    if type == 'img':
+      dogurl = requests.get("https://dog.ceo/api/breeds/image/random")
+      data2 = dogurl.json()
+      imagetoget = requests.get(data2['message']).content
+      mastodon.media_post(imagetoget, 'image/jpg')
+      #end.. or is it?
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
