69421ae7a6d14f18b87a5170992f1dbd12937dbe
strangersdangers <161281791+strangersdangers@users.noreply.github.com>
2024-03-21 01:13:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from mastodon import Mastodon
 import os
 from thingtons import keep_alive
 
-mastodon = Mastodon(access_token = os.getenv('TOKEN'))
+mastodon = Mastodon(api_base_url = os.getenv('INSTURL'), access_token = os.getenv('TOKEN'))
 
 mastodon.toot('Toot.')
 keep_alive()
