053b299f898c7732a9412fb1814974132a7e3494
strangersdangers <161281791+strangersdangers@users.noreply.github.com>
2024-03-21 23:31:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def post():
     quote = f"woke up from my break just to post this. heres a quote for yall: {data[0]['content']} -{data[0]['author']}"
     test = mastodon.toot(quote).url
     return 'posted text'
+    print(test)
   elif type == 'img':
     dogurl = requests.get("https://dog.ceo/api/breeds/image/random")
     data2 = dogurl.json()
