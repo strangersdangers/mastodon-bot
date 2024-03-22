0037429e085db79eeb372a255ea1151b40a89968
strangersdangers <161281791+strangersdangers@users.noreply.github.com>
2024-03-21 23:57:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,31 +14,38 @@ def home():
 @app.route('/post')
 def post():
   type = request.args.get('type')
+  auth = request.args.get('pass')
   quote = []
   data = []
   randomquote = []
   dogurl = []
   imagetoget = []
   image = []
+  thekey = os.getenv('PASS')
   id = []
   data2 = []
-  if type == 'text':
-    randomquote = requests.get("https://api.quotable.io/quotes/random")
-    data = randomquote.json()
-    quote = f"woke up from my break just to post this. heres a quote for yall: {data[0]['content']} -{data[0]['author']}"
-    mastodon.toot(quote)
-    return 'posted text'
-  elif type == 'img':
-    dogurl = requests.get("https://dog.ceo/api/breeds/image/random")
-    data2 = dogurl.json()
-    imagetoget = requests.get(data2["message"])
-    image = imagetoget.content
-    print(data2)
-    id = mastodon.media_post(image, 'image/png').id
-    mastodon.status_post("New dog picture for you!", media_ids=id)
-    return 'posted img'
-  elif type is None:
-    return "yooo"
-    #end.. or is it?
+  if auth is None:
+    return "one parameter missing: auth", 403
+  if auth != thekey:
+    return "auth failed, womp womp", 403
+  else:
+    if type == 'text':
+      randomquote = requests.get("https://api.quotable.io/quotes/random")
+      data = randomquote.json()
+      quote = f"woke up from my break just to post this. heres a quote for yall: {data[0]['content']} -{data[0]['author']}"
+      mastodon.toot(quote)
+      return 'posted text'
+    elif type == 'img':
+      dogurl = requests.get("https://dog.ceo/api/breeds/image/random")
+      data2 = dogurl.json()
+      imagetoget = requests.get(data2["message"])
+      image = imagetoget.content
+      print(data2)
+      id = mastodon.media_post(image, 'image/png').id
+      mastodon.status_post("New dog picture for you!", media_ids=id)
+      return 'posted img'
+    elif type is None:
+      return "yooo"
+      #end.. or is it?
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
