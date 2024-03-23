cd16c06cdbe4fcadaa1225d2db2270cd7af0a3c9
strangersdangers <161281791+strangersdangers@users.noreply.github.com>
2024-03-22 18:58:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ mastodon = Mastodon(api_base_url = os.getenv('INSTURL'), access_token = os.geten
 
 @app.route('/')
 def home():
-  testing = mastodon.account_verify_credentials().acct
   return "<h1>" + "Website proudly hosted by cyclic!" + "</h1>"
   print(f"ATTENTION, PLEASE VIEW TESTING: {testing}")
   #end
